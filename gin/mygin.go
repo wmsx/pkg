e000d75295a3469372b5b644c185e501388f71f6
lazzyq <zengqiang96@gmail.com>
2020-07-20 23:35:10 +0800

    redis

--- gin/mygin.go
+++ gin/mygin.go
@@ -6,9 +6,12 @@ import (
 	"github.com/rbcervilla/redisstore"
 )
 
-func SetUp(addr string) (err error) {
+func SetUp(addr, password string) (err error) {
 	client := redis.NewClient(&redis.Options{
 		Addr: addr,
+		Password: password,
+		PoolSize: 2,
+		DB: 1,
 	})
 	// New default RedisStore
 	if store, err = redisstore.NewRedisStore(client); err != nil {
